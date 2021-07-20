be068ce6f29fe4bc54cdbe029f5eee25c8d34ee7
patorma <pocontreras@ing.ucsc.cl>
2021-07-20 01:31:43 -0400

    se termino el tema de variables

--- src/github.com/patorma/CursoGo/variables/main.go
+++ src/github.com/patorma/CursoGo/variables/main.go
@@ -4,7 +4,13 @@ import "fmt"
 
 func main() {
 	// es opcionable poner el tipo de dato
-	var dog, cat = "🐕", "🐱"
+	//aqui con :=  digo declaro la variable y le asigno ""
+	dog, cat := "🐕", "🐱"
+	// las variables siguen siendo estaticas
+	// para reasignar un valor de una variable en go pongo signo igual
+	// cat = "gatico"
+	// si una de las variables aparece es nueva se debe usar :=
+	cat, face := "gato", "😊"
 
-	fmt.Println(dog, cat)
+	fmt.Println(dog, cat, face)
 }
